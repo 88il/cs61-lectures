da8e33bc82e353278a0a179e2d6c44ccd8cd8d3e
Iris Lang <ilang@college.harvard.edu>
2022-09-30 21:20:44 -0400

    struct alignment testing

--- datarep4/stacklayout.cc
+++ datarep4/stacklayout.cc
@@ -1,11 +1,26 @@
 #include "hexdump.hh"
 // Compile me with different compilers and optimization levels!
 
+struct x{
+    int i;
+    char c;
+};
+
+
 int main() {
     char c1 = 61;
     int i1 = 62;
     char c2 = 63;
     short s1 = 64;
+
+
+    x xval;
+
+    uintptr_t addrxval = (uintptr_t) &xval;
+    uintptr_t addrxval_c = (uintptr_t) &xval.c;
+    uintptr_t off = addrxval_c - addrxval;
+
+    printf("%zu, %zu, %zu\n", addrxval, addrxval_c, off);
 
     hexdump_named_object(c1);
     hexdump_named_object(c2);
